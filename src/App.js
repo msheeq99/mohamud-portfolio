e33aead3a2436fc9b7a37f1f5d01a0286eda984e
Mohamud <msheeq99@outlook.com>
2023-03-09 02:52:25 +0000

    about me section done, need the img to show

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
-import 'bootstrap';
+
 import Header from "./components/header";
-
-
+import Hero from "./components/hero";
+import About from "./components/about";
 export default function App() {
   return (
     <div>
       <Header />
-      
+      <Hero />
+      <About />
     </div>
   );
 }
---
+++ src/components/about.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+
+
+export default function about () {
+    return (
+        <section id="ABOUT ME">
+        
+        <div class="about-me">
+            <div class="line"></div>
+           <img src= "../images/profile.png" alt="a picture of me"></img>
+           <div class="about-text">
+            <h2>ABOUT ME</h2>
+            <h5><span> Developer </span>& Designer</h5>
+            <p>
+                I am ambitious person who has developed a mature and responsible approach to any task that i undertake.
+                while studying at UoB aspiring to become a web developer through a course. I have delivered skills like coping with mounted pressure by
+                adapting quicker, calmed & focused in challenging situations.
+            </p>
+           </div> 
+        </div>
+    </section>
+    );
+}
+
