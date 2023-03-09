23605dbdbdc0de22343a95966b95c2d59d3b61ff
Mohamud <msheeq99@outlook.com>
2023-03-09 02:04:40 +0000

    portfolio up and running with the header placed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import "./App.css";
 import 'bootstrap';
-import Header from "./components/Header";
-import Hero from "./components/Hero";
-import AboutMe from "./components/AboutMe";
-import Project from "./components/Project";
-import Footer from "./components/Footer";
-import Contact from "./components/Contact";
+import Header from "./components/header";
+
 
 export default function App() {
   return (
     <div>
       <Header />
-      <Hero />
-      <AboutMe />
-      <Project />
-      <Contact />
-      <Footer />
+      
     </div>
   );
 }
