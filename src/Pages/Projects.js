27419b50003bad015f41affa7dce5e81cdccbfcb
Mohamud <msheeq99@outlook.com>
2023-03-22 19:55:27 +0000

    rollerride added onto portfolio

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -6,6 +6,8 @@ import wds from "../assets/img/wds.png";
 import apiquiz from "../assets/img/apiquiz.png";
 import pgen from "../assets/img/pgen.png";
 import tpg from "../assets/img/tpg.png";
+import rollerRide from "../assets/img/rollerRide.png"
+
 import "./Projects.css";
 
 const Portfolio = () => {
@@ -21,84 +23,92 @@ const Portfolio = () => {
               </div>
               <div className="col-sm-4">
                 <div className="imgsec" data-aos="zoom-in">
-                  <a href="https://shrouded-woodland-67381.herokuapp.com/"> 
-                  <img src={RR} className="img-fluid" alt="1Life" />
-                  <h6>Interactive Full-Stack Application</h6>
+                  <a href="https://shrouded-woodland-67381.herokuapp.com/">
+                    <img src={RR} className="img-fluid" alt="1Life" />
+                    <h6>Interactive Full-Stack Application</h6>
                   </a>
                 </div>
-                
               </div>
               <div className="col-sm-4">
                 <div className="imgsec" data-aos="zoom-in">
-                  <a href="https://ms-texteditor.herokuapp.com/"> 
-                  <img
-                    src={Jate}
-                    className="img-fluid"
-                    alt="Jate"
-                  />
-                  <h6>Progressive Web Applications (PWA): Text Editor</h6>
+                  <a href="https://ms-texteditor.herokuapp.com/">
+                    <img src={Jate} className="img-fluid" alt="Jate" />
+                    <h6>Progressive Web Applications (PWA): Text Editor</h6>
                   </a>
                 </div>
-                
               </div>
               <div className="col-sm-4">
                 <div className="imgsec" data-aos="zoom-in">
                   <a href="https://ms-note-taker.herokuapp.com/">
-                  <img src={NoteTaker} className="img-fluid" alt="Express.js: Note Taker" />
-                  <h6>Express.js: Note Taker</h6>
+                    <img
+                      src={NoteTaker}
+                      className="img-fluid"
+                      alt="Express.js: Note Taker"
+                    />
+                    <h6>Express.js: Note Taker</h6>
                   </a>
                 </div>
-                
               </div>
               <div className="col-sm-4">
                 <div className="imgsec" data-aos="zoom-in">
                   <a href="https://msheeq99.github.io/work-day-scheduler/">
-                  <img
-                    src={wds}
-                    className="img-fluid"
-                    alt="Third-Party APIs: Work Day Scheduler"
-                  />
-                  <h6>Third-Party APIs: Work Day Scheduler</h6>
+                    <img
+                      src={wds}
+                      className="img-fluid"
+                      alt="Third-Party APIs: Work Day Scheduler"
+                    />
+                    <h6>Third-Party APIs: Work Day Scheduler</h6>
                   </a>
                 </div>
               </div>
               <div className="col-sm-4">
                 <div className="imgsec" data-aos="zoom-in">
                   <a href="https://msheeq99.github.io/web-apis-codequiz/">
-                  <img
-                    src={apiquiz}
-                    className="img-fluid"
-                    alt="Web APIs: Code Quiz"
-                  />
-                  <h6>Web APIs: Code Quiz</h6>
+                    <img
+                      src={apiquiz}
+                      className="img-fluid"
+                      alt="Web APIs: Code Quiz"
+                    />
+                    <h6>Web APIs: Code Quiz</h6>
                   </a>
                 </div>
               </div>
               <div className="col-sm-4">
                 <div className="imgsec">
-                  < a href="https://msheeq99.github.io/javascript-password-gen/">
-                  <img
-                    src={pgen}
-                    className="img-fluid"
-                    alt="JavaScript: Password Generator"
-                  />
-                  <h6>JavaScript: Password Generator</h6>
+                  <a href="https://msheeq99.github.io/javascript-password-gen/">
+                    <img
+                      src={pgen}
+                      className="img-fluid"
+                      alt="JavaScript: Password Generator"
+                    />
+                    <h6>JavaScript: Password Generator</h6>
                   </a>
                 </div>
               </div>
               <div className="col-sm-4">
                 <div className="imgsec">
-                  <a href="https://github.com/msheeq99/oop-team-profile "> 
-                  <img
-                    src={tpg}
-                    className="img-fluid"
-                    alt="Object-Oriented Programming: Team Profile Generator"
-                  />
-                  <h6>Object-Oriented Programming: Team Profile Generator</h6>
-                 </a> 
+                  <a href="https://github.com/msheeq99/oop-team-profile ">
+                    <img
+                      src={tpg}
+                      className="img-fluid"
+                      alt="Object-Oriented Programming: Team Profile Generator"
+                    />
+                    <h6>Object-Oriented Programming: Team Profile Generator</h6>
+                  </a>
                 </div>
               </div>
-             
+              <div className="col-sm-4">
+                <div className="imgsec">
+                  <a href="https://roller-coaster4.herokuapp.com/home">
+                    <img
+                      src={rollerRide}
+                      className="img-fluid"
+                      alt="RollerRide"
+                    />
+                    <h6>MERN Stack Single-Page Application</h6>
+                  </a>
+                </div>
+              </div>
             </div>
           </div>
         </div>
